51cc49c1fb693f1cb199ef0b8db58603846f124d
AbdoNayer <abdelwahabnayer20@gmail.com>
2022-11-28 17:29:26 +0200

    done

--- js/translate.js
+++ js/translate.js
@@ -24,6 +24,14 @@ $(window).on('load', function () {
         valName(`myWork`,                           `فرونت إند ديفلوبر`);
         valName(`desMe`,                            `خبره ف مجال البرمجه منذ ٦ سنوات ف المواقع والتطبيقات`);
         valName(`loading`,                          `جاري التحميل ...`);
+        valName(`part1`,                            `بداءت ف المجال من ٢٠١٦ لحد الآن .`);
+        valName(`part2`,                            `بداءت ف المجال اولا ك <span class="colorMain">UI Developer</span> .`);
+        valName(`part3`,                            `تصميم وتطوير وإدارة مواقع الويب المعقدة والأطر الداخلية والتصميم سريع الاستجابة .`);
+        valName(`part4`,                            `لدي شغف بالتعلم المستمر وتطوير نفسي في مجال <span class="colorMain">Front-End Developer</span> .`);
+        valName(`part5`,                            `ثم طورت نفسي وتعلمت الكثير من المهارات ، بما في ذلك :- <br> ( <span class="colorMain">Ionic 3</span> & <span class="colorMain">Angular</span> & <span class="colorMain">ReactJS</span> & <span class="colorMain">React Native</span> ) .`);
+        valName(`part6`,                            `لقد عملت <span class="colorMain">Front-End Developer as ( React & React Native )</span> لمدة 3 سنوات .`);
+        valName(`part7`,                            `لدي خبره ف التعامل مع ( <span class="colorMain">Adobe XD</span> & <span class="colorMain">PhotoShop</span> & <Span class="colorMain">Figma</Span> )`);
+        valName(``,                                 ``);
     }else {
         // valName(`logo`,                             `Sh3wiZo`);
         valName(`home`,                             `Home`);
